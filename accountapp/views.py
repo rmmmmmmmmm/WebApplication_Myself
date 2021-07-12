7a3f0490b3b678fdf70f400fc530d49a64d87c74
82107 <rmmmm.9@gmail.com>
2021-07-12 12:10:15 +0900

    2021.07.12. WebApplication_Myself Commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,15 +7,19 @@ from accountapp.models import HelloWorld
 
 def hello_world(request) :
 
-    temp = request.POST.get('input_text')
-
-    new_hello_world = HelloWorld()
-    new_hello_world.text = temp
-    new_hello_world.save()
-
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+
         return render(request,'accountapp/hello_world.html',
-                      context={'new_hello_world': new_hello_world})
+                      context={'hello_world_list': hello_world_list})
     else:
+        hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD!'})
+                      context={'hello_world_list': hello_world_list})
