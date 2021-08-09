6e379418bb95580ceac9bf9780b6181531f31467
82107 <rmmmm.9@gmail.com>
2021-08-09 11:32:30 +0900

    2021.08.09. WebApplication_Myself Commit 2

---
+++ commentapp/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from django.template.defaulttags import comment
+
+
+class CommentCreationForm(ModelForm):
+    class Meta:
+        model = comment
+        fields = ['content']
